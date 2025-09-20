4ba1b134284a6b066a34d2e61e02bd8ee5b8f5e5
alfaruqii <bumbblechover@gmail.com>
2025-09-20 20:37:48 +0700

    feat(maintenance): i got ddos

--- app/anime/page.tsx
+++ app/anime/page.tsx
@@ -1,29 +1,37 @@
-import HeroMediaCarousel from "@/components/hero/HeroMediaCarousel";
-import AnimeContainerCard from "../components/card/animecard/AnimeContainerCard";
-import { AnimeServiceV1, AnimeServiceV2 } from "../services";
+// Force dynamic rendering to prevent build-time API calls
+export const dynamic = 'force-dynamic';
 
-export default async function Home() {
-  const { data: dataTrending } = await AnimeServiceV2.getTrendingAnime();
-  const { data: dataRecent } = await AnimeServiceV1.getRecentEpisodeGogo();
-  const { data: dataPopular } = await AnimeServiceV2.getPopularAnime();
-
-  return (
-    <>
-      <div className="flex flex-col gap-4 min-h-fit">
-        <HeroMediaCarousel items={dataTrending.results} />
-        <div className="p-4 sm:p-0">
-          <AnimeContainerCard
-            containerTitle="Recently Updated 🎬"
-            animes={dataRecent.results}
-          />
-        </div>
-        <div className="p-4 sm:p-0">
-          <AnimeContainerCard
-            containerTitle="Most Popular 💯"
-            animes={dataPopular.results}
-          />
-        </div>
-      </div>
-    </>
-  );
+export default function AnimePage() {
+  // Since you're in maintenance mode, you can return null or a simple component
+  return null;
 }
+
+// import HeroMediaCarousel from "@/components/hero/HeroMediaCarousel";
+// import AnimeContainerCard from "../components/card/animecard/AnimeContainerCard";
+// import { AnimeServiceV1, AnimeServiceV2 } from "../services";
+//
+// export default async function Home() {
+//   const { data: dataTrending } = await AnimeServiceV2.getTrendingAnime();
+//   const { data: dataRecent } = await AnimeServiceV1.getRecentEpisodeGogo();
+//   const { data: dataPopular } = await AnimeServiceV2.getPopularAnime();
+//
+//   return (
+//     <>
+//       <div className="flex flex-col gap-4 min-h-fit">
+//         <HeroMediaCarousel items={dataTrending.results} />
+//         <div className="p-4 sm:p-0">
+//           <AnimeContainerCard
+//             containerTitle="Recently Updated 🎬"
+//             animes={dataRecent.results}
+//           />
+//         </div>
+//         <div className="p-4 sm:p-0">
+//           <AnimeContainerCard
+//             containerTitle="Most Popular 💯"
+//             animes={dataPopular.results}
+//           />
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
